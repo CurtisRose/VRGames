4fa72c5fc50adde2aae77f74c75a343c849b2445
Curtis Rose <Curtis Rose>
2017-01-06 20:00:36 -0700

    Fixed player movement. Split movement into two pieces. Control forward back left right on parent body. Looking around is for the camera which is the child of the player. Works pretty well now.

--- Games/Assets/CameraController.cs
+++ Games/Assets/CameraController.cs
@@ -3,9 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour {
-	public float cameraMovementSpeed = 2.0f;
-	public float cameraRotationSpeed = 2.0f;
-
 	public float speedH = 10.0f;
 	public float speedV = 10.0f;
 
@@ -17,42 +14,36 @@ public class CameraController : MonoBehaviour {
 	void Start () {
 		
 	}
-	
 	// Update is called once per frame
 	void Update () {
-		// Check for key presses [WASD and Arrow keys]
-		if (Input.GetButton("Forward")) {
-			Debug.Log("Moving Forward");
-			transform.Translate(cameraMovementSpeed * Vector3.forward * Time.deltaTime);
-		}
-		if (Input.GetButton("Backward")) {
-			Debug.Log("Moving Backward");
-			transform.Translate(cameraMovementSpeed * Vector3.back * Time.deltaTime);
-		}
-		if (Input.GetButton("Left")) {
-			Debug.Log("Moving Left");
-			transform.Translate(cameraMovementSpeed * Vector3.left * Time.deltaTime);
-		}
-		if (Input.GetButton("Right")) {
-			Debug.Log("Moving Right");
-			transform.Translate(cameraMovementSpeed * Vector3.right * Time.deltaTime);
-		}
 		if (Input.GetAxis("Mouse Left") < 0) {
-			Debug.Log("Looking Left");
+			//Debug.Log("Looking Left");
 			yaw += speedH * Input.GetAxis("Mouse Left") * Time.deltaTime;
 		}
 		else if (Input.GetAxis("Mouse Right") > 0) {
-			Debug.Log("Looking Right");
+			//Debug.Log("Looking Right");
 			yaw += speedH * Input.GetAxis("Mouse Right") * Time.deltaTime;
 		}
 		if (Input.GetAxis("Mouse Up") > 0) {
-			Debug.Log("Looking Up");
-			pitch -= speedV * Input.GetAxis("Mouse Up") * Time.deltaTime;
+			//Debug.Log("Looking Up");
+			if (pitch >= -90) {
+				pitch -= speedV * Input.GetAxis ("Mouse Up") * Time.deltaTime;
+			}
+			if (pitch < -90) {
+				pitch = -90;
+			}
 		}
 		else if (Input.GetAxis("Mouse Down") < 0) {
-			Debug.Log("Looking Down");
-			pitch -= speedV * Input.GetAxis("Mouse Down") * Time.deltaTime;
+			//Debug.Log("Looking Down");
+			if (pitch <= 90 ) {
+				pitch -= speedV * Input.GetAxis ("Mouse Down") * Time.deltaTime;
+			}
+			if (pitch > 90) {
+				pitch = 90;
+			}
 		}
+		//Debug.Log(pitch);
+		transform.parent.transform.eulerAngles = new Vector3(0.0f, yaw, 0.0f);
 		transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
 	}
 }
