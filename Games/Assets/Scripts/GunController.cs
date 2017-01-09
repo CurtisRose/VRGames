0d3f592131783c2638dd30f2db83077fabe2b5da
Curtis Rose <Curtis Rose>
2017-01-09 15:32:47 -0700

    Closing Issue #6. I have made an Item class that has virtual functions built in for all of the possible buttonclick son the controller. It the controller presses a button while colliding with and object that inherits the Item class, the virtual function is called in the item and the item decides what to do. This is much more clean and generic. Items decide their functionality not the controller. Obviously if grips pick up one item, it would be best if it picked up all/most items for concistency. But the item does decide.

--- Games/Assets/Scripts/GunController.cs
+++ Games/Assets/Scripts/GunController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GunController : MonoBehaviour {
+public class GunController : Item {
 	public GameObject bulletPrefab;
 	public Transform bulletSpawn;
 	public float bulletSpeed = 1000;
@@ -10,16 +10,12 @@ public class GunController : MonoBehaviour {
 	public Material onTarget;
 	public Material offTarget;
 
-	public Material singleFireColor;
-
-
-
+	private bool isHeld = false;
 
 	// Use this for initialization
 	void Start () {
 		
 	}
-		
 
 	// Update is called once per frame
 	void Update () {
@@ -66,5 +62,99 @@ public class GunController : MonoBehaviour {
 		bullet.GetComponent<Rigidbody> ().AddForce(bullet.transform.forward * bulletSpeed);
 		// Destroy the bullet after 8 seconds
 		Destroy(bullet, 8.0f);
+	}
+
+	public override void OnTriggerDown(GameObject controller) {
+		if (isHeld) {
+			Fire ();
+		}
+	}
+
+	public override void OnTriggerUp(GameObject controller) {
+		Debug.Log ("Trigger Released While Holding Gun");
+	}
+
+	public override void OnTriggerHeld(GameObject controller) {
+		Debug.Log ("Trigger Held While Holding Gun");
+	}
+
+	public override void OnHairTriggerDown(GameObject controller) {
+		Debug.Log ("Hair Trigger Pressed While Holding Gun");
+	}
+
+	public override void OnHairTriggerUp(GameObject controller) {
+		Debug.Log ("Hair Trigger Released While Holding Gun");
+	}
+
+	public override void OnHairTriggerHeld(GameObject controller) {
+		Debug.Log ("Hair Trigger Held While Holding Gun");
+	}
+
+	public override void OnGripDown(GameObject controller) {
+		if (isHeld) {
+			//Debug.Log ("Dropping Gun");
+			isHeld = false;
+
+			WandController controllerScipt = controller.GetComponent (typeof(WandController)) as WandController;
+			controllerScipt.SetControllerVisible (controller, true);
+
+			if (gameObject.transform.parent != null) {
+				gameObject.transform.parent = null;
+			}
+			gameObject.GetComponent<Rigidbody> ().useGravity = true;
+			gameObject.GetComponent<Rigidbody> ().isKinematic = false;
+			gameObject.GetComponent<Rigidbody> ().velocity = controller.GetComponent<Rigidbody>().velocity;
+			gameObject.GetComponent<Rigidbody> ().angularVelocity = controller.GetComponent<Rigidbody>().angularVelocity;
+
+		} else {
+			//Debug.Log ("Picking up Gun");
+			isHeld = true;
+			WandController controllerScipt = controller.GetComponent (typeof(WandController)) as WandController;
+			controllerScipt.SetControllerVisible (controller, false);
+
+			gameObject.transform.parent = controller.transform;
+			gameObject.GetComponent<Rigidbody> ().useGravity = false;
+			gameObject.GetComponent<Rigidbody> ().isKinematic = true;
+		
+			//Rotate the gun properly
+			Quaternion newRotation = Quaternion.Euler (-75, 180, 0);
+			gameObject.transform.rotation = controller.transform.rotation * newRotation;
+
+			// Position the gun properly
+			Vector3 newPosition = new Vector3(0.0f, -0.25f, 0.1f);
+			gameObject.transform.localPosition = newPosition;
+		}
+	}
+
+	public override void OnGripUp(GameObject controller) {
+		Debug.Log ("Grip Released While Holding Gun");
+	}
+
+	public override void OnGripHeld(GameObject controller) {
+		Debug.Log ("Grip Held While Holding Gun");
+	}
+
+	public override void OnTouchpadDown(GameObject controller) {
+		Debug.Log ("Touchpad Pressed While Holding Gun");
+	}
+
+	public override void OnTouchpadUp(GameObject controller) {
+		Debug.Log ("Touchpad Released While Holding Gun");
+	}
+
+	public override void OnTouchpadHeld(GameObject controller) {
+		Debug.Log ("Touchpad Held While Holding Gun");
+	}
+
+	public override void OnMenuDown(GameObject controller) {
+		Debug.Log ("Menu Pressed While Holding Gun");
+	}
+
+	public override void OnMenuUp(GameObject controller) {
+		Debug.Log ("Menu Released While Holding Gun");
+	}
+
+	public override void OnMenuHeld(GameObject controller) {
+		Debug.Log ("Menu Held While Holding Gun");
 	}
 }
--- Games/Assets/Scripts/WandController.cs
+++ Games/Assets/Scripts/WandController.cs
@@ -2,70 +2,115 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class WandController : MonoBehaviour {
-/*	private Valve.VR.EVRButtonId gripButton = Valve.VR.EVRButtonId.k_EButton_Grip;
-	private Valve.VR.EVRButtonId triggerButton = Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger;
-	private Valve.VR.EVRButtonId menuButton = Valve.VR.EVRButtonId.k_EButton_ApplicationMenu;
-	private Valve.VR.EVRButtonId dashboardButton = Valve.VR.EVRButtonId.k_EButton_Dashboard_Back;
-	private Valve.VR.EVRButtonId downButton = Valve.VR.EVRButtonId.k_EButton_DPad_Up;
-	private Valve.VR.EVRButtonId upButton = Valve.VR.EVRButtonId.k_EButton_DPad_Down;
-	private Valve.VR.EVRButtonId LeftButton = Valve.VR.EVRButtonId.k_EButton_DPad_Left;
-	private Valve.VR.EVRButtonId RightButton = Valve.VR.EVRButtonId.k_EButton_DPad_Right;
-*/
-
-	Valve.VR.EVRButtonId[] buttonIds = new Valve.VR.EVRButtonId[] {
-		Valve.VR.EVRButtonId.k_EButton_ApplicationMenu,
-		Valve.VR.EVRButtonId.k_EButton_Grip,
-		Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad,
-		Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger
-	};
-
-	Valve.VR.EVRButtonId[] axisIds = new Valve.VR.EVRButtonId[] {
-		Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad,
-		Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger
-	};
-
-	private SteamVR_Controller.Device controller;
 	private SteamVR_TrackedObject trackedObj;
-
-	// Use this for initialization
-	void Start () {
-		trackedObj = GetComponent<SteamVR_TrackedObject> ();
-		controller =  SteamVR_Controller.Input((int) trackedObj.index);
+	private GameObject collidingObject; // Object being touched by controller.
+	private GameObject objectInHand; // Object held by controller.
+	private SteamVR_Controller.Device controller {
+		get { return SteamVR_Controller.Input ((int)trackedObj.index); }
 	}
-	
+
+
+
+	void Awake() {
+		//Debug.Log ("Testing ControllerGrabItem Awake()");
+		trackedObj = GetComponent<SteamVR_TrackedObject> ();
+	}
+
+	// Sets up other collider as potential grab target.
+	public void OnTriggerEnter(Collider other) {
+		//Debug.Log ("Touching an object");
+		SetCollidingObject (other);
+	}
+
+	// Makes sure that the target is still set.
+	public void OnTriggerStay(Collider other) {
+		//Debug.Log ("Still Touching an object");
+		SetCollidingObject (other);
+	}
+
+	// Removes other as a potential grab target.
+	public void OnTriggerExit(Collider other) {
+		//Debug.Log ("Stopped Touching an object");
+		if (!collidingObject) {
+			return;
+		}
+		collidingObject = null;
+	}
+
+	private void SetCollidingObject(Collider col) {
+		if (collidingObject || !col.GetComponent<Rigidbody> ()) {
+			return;
+		}
+		//Debug.Log ("Setting colliding object: " + col.gameObject);
+		collidingObject = col.gameObject;
+	}
+		
 	// Update is called once per frame
 	void Update () {
-		/*if (controller.GetAxis () != Vector2.zero) {
-			//Debug.Log (gameObject.name + " " + controller.GetAxis ());
+		if (controller.GetPressDown (SteamVR_Controller.ButtonMask.Grip) && collidingObject) {
+			Item objectInHandScript = collidingObject.GetComponent (typeof(Item)) as Item;
+			objectInHandScript.OnGripDown (gameObject);
 		}
-		if (controller.GetHairTriggerDown ()) {
-			//Debug.Log (gameObject.name + "Trigger Pressed");
-		}
-		if (controller.GetHairTriggerUp ()) {
-			//Debug.Log (gameObject.name + "Trigger Released");
-		}
-		if (controller.GetPressDown (SteamVR_Controller.ButtonMask.Grip)) {
-			//Debug.Log (gameObject.name + "Grip Pressed");
-		}
-		if (controller.GetPressUp (SteamVR_Controller.ButtonMask.Grip)) {
-			//Debug.Log (gameObject.name + "Grip Released");
-		}
-		// Need to pull harder on the trigger to activate this.
-		if (controller.GetPressDown (SteamVR_Controller.ButtonMask.Trigger)) {
-			//Debug.Log (gameObject.name + "New Trigger Pressed");
-		}
-		if (controller.GetPressUp (SteamVR_Controller.ButtonMask.Trigger)) {
-			//Debug.Log (gameObject.name + "New Trigger Released");
-		}*/
 	}
 
-	void OnCollisionEnter (Collision col) {
-		/*if (col.gameObject.CompareTag ("Item")) {
-			Debug.Log ("Touching an item");
-		} else {
-			Debug.Log ("Touching something else");
-		}*/
+	public void SetControllerVisible(GameObject controller, bool isVisible) {
+		foreach (SteamVR_RenderModel model in controller.GetComponentsInChildren<SteamVR_RenderModel>()) {
+			foreach (var child in model.GetComponentsInChildren<MeshRenderer>()) {
+				child.enabled = isVisible;
+			}
+		}
 	}
 }
+
+
+
+
+
+
+////////////////////////////////////////////////////////////////////
+/// Random things to keep for later maybe
+/// 
+/*	
+private Valve.VR.EVRButtonId gripButton = Valve.VR.EVRButtonId.k_EButton_Grip;
+private Valve.VR.EVRButtonId triggerButton = Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger;
+private Valve.VR.EVRButtonId menuButton = Valve.VR.EVRButtonId.k_EButton_ApplicationMenu;
+private Valve.VR.EVRButtonId dashboardButton = Valve.VR.EVRButtonId.k_EButton_Dashboard_Back;
+private Valve.VR.EVRButtonId downButton = Valve.VR.EVRButtonId.k_EButton_DPad_Up;
+private Valve.VR.EVRButtonId upButton = Valve.VR.EVRButtonId.k_EButton_DPad_Down;
+private Valve.VR.EVRButtonId LeftButton = Valve.VR.EVRButtonId.k_EButton_DPad_Left;
+private Valve.VR.EVRButtonId RightButton = Valve.VR.EVRButtonId.k_EButton_DPad_Right;
+
+Valve.VR.EVRButtonId[] buttonIds = new Valve.VR.EVRButtonId[] {
+	Valve.VR.EVRButtonId.k_EButton_ApplicationMenu,
+	Valve.VR.EVRButtonId.k_EButton_Grip,
+	Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad,
+	Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger};
+
+Valve.VR.EVRButtonId[] axisIds = new Valve.VR.EVRButtonId[] {
+	Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad,
+	Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger
+};
+
+if (controller.GetAxis () != Vector2.zero) {
+	//Debug.Log (gameObject.name + " " + controller.GetAxis ());
+}
+if (controller.GetHairTriggerDown ()) {
+	//Debug.Log (gameObject.name + "Trigger Pressed");
+}
+if (controller.GetHairTriggerUp ()) {
+	//Debug.Log (gameObject.name + "Trigger Released");
+}
+if (controller.GetPressDown (SteamVR_Controller.ButtonMask.Grip)) {
+	//Debug.Log (gameObject.name + "Grip Pressed");
+}
+if (controller.GetPressUp (SteamVR_Controller.ButtonMask.Grip)) {
+	//Debug.Log (gameObject.name + "Grip Released");
+}
+// Need to pull harder on the trigger to activate this.
+if (controller.GetPressDown (SteamVR_Controller.ButtonMask.Trigger)) {
+	//Debug.Log (gameObject.name + "New Trigger Pressed");
+}
+if (controller.GetPressUp (SteamVR_Controller.ButtonMask.Trigger)) {
+	//Debug.Log (gameObject.name + "New Trigger Released");
+}*/
