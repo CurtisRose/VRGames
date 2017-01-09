56ec0da59b274786c8620bb33c7b5a93827a95d1
Curtis Rose <Curtis Rose>
2017-01-08 18:35:54 -0700

    Closing issue #2 and #1. You can now pick up objects with the controller if they have a rigidbody, collider, and tagged as an Item. Also adding a better gitignore and removing things from the repo that arent necessary

---
+++ Games/Assets/Scripts/ControllerGrabItem.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerGrabItem : MonoBehaviour {
+	private SteamVR_TrackedObject trackedObj;
+	private GameObject collidingObject; // Object being touched by controller.
+	private GameObject objectInHand; // Object held by controller.
+	private SteamVR_Controller.Device controller {
+		get { return SteamVR_Controller.Input ((int)trackedObj.index); }
+	}
+
+
+	void Awake() {
+		//Debug.Log ("Testing ControllerGrabItem Awake()");
+		trackedObj = GetComponent<SteamVR_TrackedObject> ();
+	}
+
+	// Sets up other collider as potential grab target.
+	public void OnTriggerEnter(Collider other) {
+		//Debug.Log ("Touching an object");
+		SetCollidingObject (other);
+	}
+
+	// Makes sure that the target is still set.
+	public void OnTriggerStay(Collider other) {
+		//Debug.Log ("Still Touching an object");
+		SetCollidingObject (other);
+	}
+
+	// Removes other as a potential grab target.
+	public void OnTriggerExit(Collider other) {
+		//Debug.Log ("Stopped Touching an object");
+		if (!collidingObject) {
+			return;
+		}
+		collidingObject = null;
+	}
+
+	private void SetCollidingObject(Collider col) {
+		if (collidingObject || !col.GetComponent<Rigidbody> ()) {
+			return;
+		}
+		//Debug.Log ("Setting colliding object: " + col.gameObject);
+		collidingObject = col.gameObject;
+	}
+
+	private void GrabObject() {
+		//Debug.Log("Grabbing object: " + collidingObject.name);
+		objectInHand = collidingObject;
+		collidingObject = null;
+		var joint = AddFixedJoint ();
+		joint.connectedBody = objectInHand.GetComponent<Rigidbody> ();
+	}
+
+	private void ReleaseObject() {
+		if (GetComponent<FixedJoint> ()) {
+			//Debug.Log ("Releasing object: " + objectInHand.name);
+			GetComponent<FixedJoint> ().connectedBody = null;
+			Destroy (GetComponent<FixedJoint> ());
+			objectInHand.GetComponent<Rigidbody> ().velocity = controller.velocity;
+			objectInHand.GetComponent<Rigidbody> ().angularVelocity = controller.angularVelocity;
+			objectInHand = null;
+		}
+	}
+
+	private FixedJoint AddFixedJoint() {
+		FixedJoint fx = gameObject.AddComponent<FixedJoint> ();
+		fx.breakForce = 20000;
+		fx.breakTorque = 20000;
+		return fx;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (controller.GetHairTriggerDown ()) {
+			if (collidingObject && collidingObject.CompareTag("Item")) {
+				GrabObject ();
+			}
+		}
+		if (controller.GetHairTriggerUp ()) {
+			ReleaseObject ();
+		}
+	}
+}
---
+++ Games/Assets/Scripts/WandController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class WandController : MonoBehaviour {
+/*	private Valve.VR.EVRButtonId gripButton = Valve.VR.EVRButtonId.k_EButton_Grip;
+	private Valve.VR.EVRButtonId triggerButton = Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger;
+	private Valve.VR.EVRButtonId menuButton = Valve.VR.EVRButtonId.k_EButton_ApplicationMenu;
+	private Valve.VR.EVRButtonId dashboardButton = Valve.VR.EVRButtonId.k_EButton_Dashboard_Back;
+	private Valve.VR.EVRButtonId downButton = Valve.VR.EVRButtonId.k_EButton_DPad_Up;
+	private Valve.VR.EVRButtonId upButton = Valve.VR.EVRButtonId.k_EButton_DPad_Down;
+	private Valve.VR.EVRButtonId LeftButton = Valve.VR.EVRButtonId.k_EButton_DPad_Left;
+	private Valve.VR.EVRButtonId RightButton = Valve.VR.EVRButtonId.k_EButton_DPad_Right;
+*/
+
+	Valve.VR.EVRButtonId[] buttonIds = new Valve.VR.EVRButtonId[] {
+		Valve.VR.EVRButtonId.k_EButton_ApplicationMenu,
+		Valve.VR.EVRButtonId.k_EButton_Grip,
+		Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad,
+		Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger
+	};
+
+	Valve.VR.EVRButtonId[] axisIds = new Valve.VR.EVRButtonId[] {
+		Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad,
+		Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger
+	};
+
+	private SteamVR_Controller.Device controller;
+	private SteamVR_TrackedObject trackedObj;
+
+	// Use this for initialization
+	void Start () {
+		trackedObj = GetComponent<SteamVR_TrackedObject> ();
+		controller =  SteamVR_Controller.Input((int) trackedObj.index);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		/*if (controller.GetAxis () != Vector2.zero) {
+			//Debug.Log (gameObject.name + " " + controller.GetAxis ());
+		}
+		if (controller.GetHairTriggerDown ()) {
+			//Debug.Log (gameObject.name + "Trigger Pressed");
+		}
+		if (controller.GetHairTriggerUp ()) {
+			//Debug.Log (gameObject.name + "Trigger Released");
+		}
+		if (controller.GetPressDown (SteamVR_Controller.ButtonMask.Grip)) {
+			//Debug.Log (gameObject.name + "Grip Pressed");
+		}
+		if (controller.GetPressUp (SteamVR_Controller.ButtonMask.Grip)) {
+			//Debug.Log (gameObject.name + "Grip Released");
+		}
+		// Need to pull harder on the trigger to activate this.
+		if (controller.GetPressDown (SteamVR_Controller.ButtonMask.Trigger)) {
+			//Debug.Log (gameObject.name + "New Trigger Pressed");
+		}
+		if (controller.GetPressUp (SteamVR_Controller.ButtonMask.Trigger)) {
+			//Debug.Log (gameObject.name + "New Trigger Released");
+		}*/
+	}
+
+	void OnCollisionEnter (Collision col) {
+		/*if (col.gameObject.CompareTag ("Item")) {
+			Debug.Log ("Touching an item");
+		} else {
+			Debug.Log ("Touching something else");
+		}*/
+	}
+}
