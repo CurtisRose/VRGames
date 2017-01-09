a12c49116a0d3475f1aedce1337c8f5dcf916c1b
Curtis Rose <Curtis Rose>
2017-01-08 22:42:36 -0700

    Closing issue #3 Be able to pick up a weapon. You can now pick up a somewhat functioning weapon. Shoots bullets at at a single shot rate. Aimer turns colors when aimed at a target.

--- Games/Assets/Scripts/ControllerGrabItem.cs
+++ Games/Assets/Scripts/ControllerGrabItem.cs
@@ -46,40 +46,75 @@ public class ControllerGrabItem : MonoBehaviour {
 	}
 
 	private void GrabObject() {
-		//Debug.Log("Grabbing object: " + collidingObject.name);
 		objectInHand = collidingObject;
 		collidingObject = null;
-		var joint = AddFixedJoint ();
-		joint.connectedBody = objectInHand.GetComponent<Rigidbody> ();
+
+		objectInHand.transform.parent = gameObject.transform;
+		objectInHand.GetComponent<Rigidbody> ().useGravity = false;
+		objectInHand.GetComponent<Rigidbody> ().isKinematic = true;
+	}
+
+	private void GrabWeapon() {
+		//Debug.Log("Grabbing weapon: " + collidingObject.name);
+		objectInHand = collidingObject;
+		collidingObject = null;
+
+		objectInHand.transform.parent = gameObject.transform;
+		objectInHand.GetComponent<Rigidbody> ().useGravity = false;
+		objectInHand.GetComponent<Rigidbody> ().isKinematic = true;
+
+		//Rotate the gun properly
+		Quaternion newRotation = Quaternion.Euler (-75, 180, 0);
+		objectInHand.transform.rotation = controller.transform.rot * newRotation;
+
+		// Position the gun properly
+		Vector3 newPosition = new Vector3(0.0f, -0.25f, 0.1f);
+		objectInHand.transform.localPosition = newPosition;
+
+		// Make controller dissapear
+		SetControllerVisible(gameObject, false);
 	}
 
 	private void ReleaseObject() {
-		if (GetComponent<FixedJoint> ()) {
-			//Debug.Log ("Releasing object: " + objectInHand.name);
-			GetComponent<FixedJoint> ().connectedBody = null;
-			Destroy (GetComponent<FixedJoint> ());
-			objectInHand.GetComponent<Rigidbody> ().velocity = controller.velocity;
-			objectInHand.GetComponent<Rigidbody> ().angularVelocity = controller.angularVelocity;
-			objectInHand = null;
+		// Make controller reappear
+		SetControllerVisible(gameObject, true);
+		if (objectInHand.transform.parent != null) {
+			objectInHand.transform.parent = null;
 		}
+		objectInHand.GetComponent<Rigidbody> ().useGravity = true;
+		objectInHand.GetComponent<Rigidbody> ().isKinematic = false;
+		objectInHand.GetComponent<Rigidbody> ().velocity = controller.velocity;
+		objectInHand.GetComponent<Rigidbody> ().angularVelocity = controller.angularVelocity;
+		objectInHand = null;
 	}
 
 	private FixedJoint AddFixedJoint() {
 		FixedJoint fx = gameObject.AddComponent<FixedJoint> ();
-		fx.breakForce = 20000;
+		fx.breakForce = 20000; // Mathf.Infinity instead maybe
 		fx.breakTorque = 20000;
 		return fx;
 	}
 
 	// Update is called once per frame
 	void Update () {
-		if (controller.GetHairTriggerDown ()) {
+		if (controller.GetPressDown (SteamVR_Controller.ButtonMask.Grip) && !objectInHand) {
 			if (collidingObject && collidingObject.CompareTag("Item")) {
 				GrabObject ();
 			}
+			if (collidingObject && collidingObject.CompareTag("Weapon")) {
+				GrabWeapon ();
+			}
 		}
-		if (controller.GetHairTriggerUp ()) {
+		else if (controller.GetPressDown (SteamVR_Controller.ButtonMask.Grip) && objectInHand) {
 			ReleaseObject ();
+		}
+	}
+
+	void SetControllerVisible(GameObject controller, bool isVisible) {
+		foreach (SteamVR_RenderModel model in controller.GetComponentsInChildren<SteamVR_RenderModel>()) {
+			foreach (var child in model.GetComponentsInChildren<MeshRenderer>()) {
+				child.enabled = isVisible;
+			}
 		}
 	}
 }
--- Games/Assets/Scripts/GunController.cs
+++ Games/Assets/Scripts/GunController.cs
@@ -3,163 +3,68 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GunController : MonoBehaviour {
-	private float maxFOV = 60;
-	private float minFOV = 40;
-	private float zoomSpeed = 2;
-
 	public GameObject bulletPrefab;
 	public Transform bulletSpawn;
-	public float bulletSpeed = 2;
+	public float bulletSpeed = 1000;
 
 	public Material onTarget;
 	public Material offTarget;
-	public Material automaticFireColor;
-	public Material burstFireColor;
+
 	public Material singleFireColor;
 
-	public GameObject rifleTargetAim;
-	public GameObject rifleTargetHip;
 
-	private int fireToggle = 0; //0 single, 1 burst, 2 auto
-	private float time;
-	private float rateOfFire = 0.1f;
-	private int burstCount = 50;
-	private float aimSpeed = 0.04f;
-
-	float timeToGo;
-
-	static Vector3 aimingDownSights = new Vector3(-.264f, .2f, 0.1f);
 
 
 	// Use this for initialization
 	void Start () {
-		time = Time.time;
+		
 	}
 		
 
 	// Update is called once per frame
 	void Update () {
-		// Toggling Rate of Fire
-		if (Input.GetKeyDown(KeyCode.E)) {
-			if (fireToggle == 0) {
-				fireToggle = 1;
-				transform.GetChild (4).GetComponent<Renderer>().material = burstFireColor;
-			}
-			else if (fireToggle == 1) {
-				fireToggle = 2;
-				transform.GetChild (4).GetComponent<Renderer>().material = automaticFireColor;
-			}
-			else if (fireToggle == 2) {
-				fireToggle = 0;
-				transform.GetChild (4).GetComponent<Renderer>().material = singleFireColor;
-			}
-		}
-
 		// Shooting
-		if (Input.GetMouseButtonDown(0)) {
-			//Debug.Log("Shooting");
-			if (fireToggle == 0) {
-				Fire ();
-			} else if (fireToggle == 1) {
-				for (int i = 0; i <= burstCount; i++) {
-					Fire ();
-				}
-			}
-		} else if (Input.GetMouseButton(0) && fireToggle == 2) {
-			//Debug.Log("Shooting");
+		if (gameObject.transform.parent != null && 
+			SteamVR_Controller.Input ((int)gameObject.transform.parent.gameObject.GetComponent<SteamVR_TrackedObject> ().index).GetHairTriggerDown ()) {
+			//Debug.Log("Attempting To Shoot");
 			Fire();
 		}
 
-		// Aiming Down Sights
-		if (Input.GetMouseButton (1)) {
-			//Debug.Log ("Aiming In");
-			Vector3 velocity = Vector3.zero;
-			transform.position = Vector3.SmoothDamp (transform.position, 
-				rifleTargetAim.transform.position, ref velocity, aimSpeed);
-		}
-		else {
-			//Debug.Log ("Aiming Out");
-			Vector3 velocity = Vector3.zero;
-			transform.position = Vector3.SmoothDamp (transform.position, 
-				rifleTargetHip.transform.position, ref velocity, aimSpeed);
-		}
-		if (Input.GetMouseButton(1)) {
-			if (Camera.main.fieldOfView >= minFOV) {
-				if (Camera.main.fieldOfView - zoomSpeed < minFOV) {
-					Camera.main.fieldOfView = minFOV;
-				} else {
-					Camera.main.fieldOfView -= zoomSpeed;
-				}
-			}
-		} else {
-			if (Camera.main.fieldOfView <= maxFOV) {
-				if (Camera.main.fieldOfView + zoomSpeed > maxFOV) {
-					Camera.main.fieldOfView = maxFOV;
-				} else {
-					Camera.main.fieldOfView += zoomSpeed;
-				}
-			}
-		}
-
 		// Raycasting to change sight colors
-		Ray ray = new Ray(transform.GetChild (0).GetComponent<Transform>().position, 
-			transform.GetChild (0).GetComponent<Transform>().forward);
+		Ray ray = new Ray(transform.GetChild (3).GetComponent<Transform>().position, 
+			transform.GetChild (3).GetComponent<Transform>().forward);
 		RaycastHit hit;
 		if (Physics.Raycast (ray, out hit, 100.0f)) {
 			if (hit.transform.tag == "Target") {
-				transform.GetChild(0).GetComponent<Renderer>().material = onTarget;
-				transform.GetChild(1).GetComponent<Renderer>().material = onTarget;
-				transform.GetChild(2).GetComponent<Renderer>().material = onTarget;
+				transform.GetChild(0).GetChild(0).GetComponent<Renderer>().material = onTarget;
+				transform.GetChild(0).GetChild(1).GetComponent<Renderer>().material = onTarget;
+				transform.GetChild(0).GetChild(2).GetComponent<Renderer>().material = onTarget;
 			}
 			else if (hit.transform.tag == "Bullet") {
 
 			}
 			else {
-				transform.GetChild(0).GetComponent<Renderer>().material = offTarget;
-				transform.GetChild(1).GetComponent<Renderer>().material = offTarget;
-				transform.GetChild(2).GetComponent<Renderer>().material = offTarget;
+				transform.GetChild(0).GetChild(0).GetComponent<Renderer>().material = offTarget;
+				transform.GetChild(0).GetChild(1).GetComponent<Renderer>().material = offTarget;
+				transform.GetChild(0).GetChild(2).GetComponent<Renderer>().material = offTarget;
 			}
 		} else {
-			transform.GetChild(0).GetComponent<Renderer>().material = offTarget;
-			transform.GetChild(1).GetComponent<Renderer>().material = offTarget;
-			transform.GetChild(2).GetComponent<Renderer>().material = offTarget;
+			transform.GetChild(0).GetChild(0).GetComponent<Renderer>().material = offTarget;
+			transform.GetChild(0).GetChild(1).GetComponent<Renderer>().material = offTarget;
+			transform.GetChild(0).GetChild(2).GetComponent<Renderer>().material = offTarget;
 		}
 	}
 
 	void Fire() {
-		if (fireToggle == 2) { // Automatic
-			if (Time.realtimeSinceStartup - time > rateOfFire) {
-				time = Time.realtimeSinceStartup;
-				// Create the Bullet from the Bullet Prefab
-				GameObject bullet = Instantiate (
-					bulletPrefab,
-					bulletSpawn.position,
-					bulletSpawn.rotation);
-				// Add velocity to the bullet
-				bullet.GetComponent<Rigidbody> ().velocity = bullet.transform.forward * Time.deltaTime * bulletSpeed;
-				// Destroy the bullet after 8 seconds
-				Destroy(bullet, 8.0f);
-			}
-		}else if (fireToggle == 1) { // Burst Fire
-			// Create the Bullet from the Bullet Prefab
-			GameObject bullet = Instantiate (
-				bulletPrefab,
-				bulletSpawn.position,
-				bulletSpawn.rotation);
-			// Add velocity to the bullet
-			bullet.GetComponent<Rigidbody> ().velocity = bullet.transform.forward * Time.deltaTime * bulletSpeed;
-			// Destroy the bullet after 8 seconds
-			Destroy(bullet, 8.0f);
-		}else if (fireToggle == 0) { // Single Shot
-			// Create the Bullet from the Bullet Prefab
-			GameObject bullet = Instantiate (
-				bulletPrefab,
-				bulletSpawn.position,
-				bulletSpawn.rotation);
-			// Add velocity to the bullet
-			bullet.GetComponent<Rigidbody> ().velocity = bullet.transform.forward * Time.deltaTime * bulletSpeed;
-			// Destroy the bullet after 8 seconds
-			Destroy(bullet, 8.0f);
-		}
+		// Create the Bullet from the Bullet Prefab
+		//Debug.Log("Firing");
+		GameObject bullet = Instantiate (
+			bulletPrefab,
+			bulletSpawn.position,
+			bulletSpawn.rotation);
+		// Add velocity to the bullet
+		bullet.GetComponent<Rigidbody> ().AddForce(bullet.transform.forward * bulletSpeed);
+		// Destroy the bullet after 8 seconds
+		Destroy(bullet, 8.0f);
 	}
 }
