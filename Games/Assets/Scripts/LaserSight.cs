56b7b182d0f2336c2c9bd5a1c1143bcbd4523cdf
Curtis Rose <Curtis Rose>
2017-01-09 13:28:00 -0700

    Added a laser dot where the laser hits to make it more clear what the laser is hitting.

--- Games/Assets/Scripts/LaserSight.cs
+++ Games/Assets/Scripts/LaserSight.cs
@@ -10,19 +10,23 @@ public class LaserSight : MonoBehaviour {
 	void Start () {
 		//Debug.Log(transform.GetChild (0).name);
 		transform.GetChild (0).gameObject.SetActive (false);
+		transform.GetChild(1).gameObject.SetActive (false);
 	}
 
 	private void ShowLaser(RaycastHit hit) {
 		//Debug.Log ("hit distance: " + hit.distance);
 		transform.GetChild(0).position = Vector3.Lerp (transform.position, hitPoint, 0.5f);
 		transform.GetChild(0).LookAt (hitPoint);
+		transform.GetChild(1).gameObject.SetActive (true);
+		transform.GetChild (1).position = hitPoint;
 		transform.GetChild(0).localScale = new Vector3 (
 			transform.GetChild(0).localScale.x,
 			transform.GetChild(0).localScale.y,
 			hit.distance/(transform.localScale.z * transform.parent.localScale.z));
 	}
 	private void ShowLaserInfinite() {
-		Debug.Log (transform.forward);
+		//Debug.Log (transform.forward);
+		transform.GetChild(1).gameObject.SetActive (false);
 		transform.GetChild(0).position = Vector3.Lerp (transform.position, transform.position + (-1 * transform.forward * maxLaserDistance), 0.5f);
 		transform.GetChild (0).LookAt (transform.position);
 		transform.GetChild(0).localScale = new Vector3 (
@@ -35,11 +39,11 @@ public class LaserSight : MonoBehaviour {
 		if (gameObject.transform.parent.parent != null) {
 			if (SteamVR_Controller.Input ((int)gameObject.transform.parent.parent.gameObject.GetComponent<SteamVR_TrackedObject> ().index).GetPressDown (SteamVR_Controller.ButtonMask.Touchpad)) {
 				if (laserOn) {
-					Debug.Log ("Turning off Laser");
+					//Debug.Log ("Turning off Laser");
 					laserOn = false;
 					transform.GetChild(0).gameObject.SetActive (false);
 				} else {
-					Debug.Log ("Turning on Laser");
+					//Debug.Log ("Turning on Laser");
 					laserOn = true;
 					transform.GetChild(0).gameObject.SetActive (true);
 				}
